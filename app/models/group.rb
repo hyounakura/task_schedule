faeca1ed5ee7bdf7ec3b00a8e73689f712d6dee0
hyounakura <leopardtender@gmail.com>
2020-08-03 16:07:23 +0900

    AddRubocop

--- app/models/group.rb
+++ app/models/group.rb
@@ -11,22 +11,22 @@ module Calender
     calendar = {}
     start_day = today.beginning_of_month
     start_week = [1, 2, 3, 4, 5, 6, 7]
-    end_week = ["", "", "", "", "", "", ""]
+    end_week = ['', '', '', '', '', '', '']
     end_day = today.end_of_month
     middle_week = []
     middle_days = []
-  
+
     start_week.count.times do |index|
-      index >= start_day.wday ? start_week[index] = index - start_day.wday + 1 : start_week[index] = ""
+      start_week[index] = index >= start_day.wday ? index - start_day.wday + 1 : ''
     end
     (end_day.wday + 1).times do |index|
       end_week.unshift(end_day.day - index)
       end_week.delete_at(-1)
     end
-  
+
     week_day = 7
     last_day = start_week[start_week.count - 1]
-    ((end_week[0] - start_week[start_week.count - 1] - 1) / week_day).times do |index|
+    ((end_week[0] - start_week[start_week.count - 1] - 1) / week_day).times do |_index|
       middle_days = []
       week_day.times do |day|
         middle_days.push last_day + day + 1
